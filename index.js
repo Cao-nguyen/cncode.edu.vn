08c2def9dc6b089862f2b3165580f37e72bc400a
Cao-nguyen <chinhphucnguvan@gmail.com>
2024-10-19 20:21:53 +0700

    c

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import configViewEngine from './src/configs/viewEngine.js'
+import path from 'path'
 import initWebRoutes from './src/routes/web.js'
+import { fileURLToPath } from 'url'
 
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3001
 
-// config view engine
-configViewEngine(app)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 // init web routes
 initWebRoutes(app)
